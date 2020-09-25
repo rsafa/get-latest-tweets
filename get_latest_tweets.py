76a16e83afacd695480e3beb590d08f92f3b81a4
Ramin Safa <70813395+rsafa@users.noreply.github.com>
2020-09-26 01:08:31 +0330

    Add files via upload

---
+++ get_latest_tweets.py
@@ -0,0 +1,36 @@
+import tweepy
+import pandas as pd
+import time
+
+consumer_key = 'CONSUMER KEY'
+consumer_secret = 'CONSUMER KEY'
+access_token = 'ACCESS TOKEN'
+access_token_secret = 'ACCESS TOKEN SECRET'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+tweets = []
+
+
+def query_to_csv(text_query, count):
+    try:
+        tweets = tweepy.Cursor(api.search, q=text_query).items(count)
+        tweets_list = [[tweet.id, tweet.created_at, tweet.text] for tweet in tweets]
+
+        # tweet information
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID', 'Datetime', 'Text'])
+
+        tweets_df.to_csv('{}-tweets.csv'.format(text_query), sep=',', index=False)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
+
+
+# X is number of tweets to be retrieved
+text_query = 'YOUR QUERY'
+count = X
+
+query_to_csv(text_query, count)
